04b6fa0f4b2bb4b8bbac4be65e1a84fec6d2ca09
BismaYousuf <bismayousuf540@gmail.com>
2025-01-21 09:23:32 -0600

    up--7

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,6 +1,36 @@
-import ProductDetail from '@/components/ProductDetails'
 
-export default function ProductPage({ params }: { params: { id: string } }) {
-  return <ProductDetail id={params.id} />
+
+// import ProductDetail from "@/app/shop/[id]/page"
+
+// interface ProductPageProps {
+//   params: {
+//     id: string
+//   }
+// }
+
+// export default async function ProductPage({ params }: ProductPageProps) {
+//   return <ProductDetail id={params.id} />
+// }
+
+
+// app/shop/[id]/page.tsx
+interface ProductDetailProps {
+  params: {
+    id: string;
+  };
 }
 
+export default async function ProductDetail({ params }: ProductDetailProps) {
+  const { id } = params;
+
+  // Fetch product data based on the `id`
+  // const product = await fetchProductById(id);
+
+  return (
+    <div>
+      <h1>Product Detail Page</h1>
+      <p>Product ID: {id}</p>
+      {/* Render product details here */}
+    </div>
+  );
+}
